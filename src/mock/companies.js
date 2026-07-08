d65228fadb5745bc8f8e84891577aaa888c87d1e
aksths <aksths161@gmail.com>
2026-07-08 17:01:24 +0900

    feat: list components works

---
+++ src/mock/companies.js
@@ -0,0 +1,37 @@
+const companies = [
+  {
+    id: 1,
+    name: '블루코드',
+    category: '기계장비',
+    logo: '/images/bluecode.png',
+    selected: false,
+  },
+  {
+    id: 2,
+    name: '씨코드',
+    category: '플랫폼',
+    logo: '/images/ccode.png',
+    selected: false,
+  },
+  {
+    id: 3,
+    name: '코드스타일',
+    category: '이동통신',
+    logo: '/images/codestyle.png',
+    selected: true,
+  },
+  {
+    id: 4,
+    name: '에이코드',
+    category: '솔루션',
+    logo: '/images/acode.png',
+    selected: true,
+  },
+  {
+    id: 5,
+    name: '엔코드',
+    category: '전자기기',
+    logo: '/images/ncode.png',
+    selected: true,
+  },
+];
