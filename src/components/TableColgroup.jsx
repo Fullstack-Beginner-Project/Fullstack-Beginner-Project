036bb3ed5acbeaf7e17efc5c22f70e09d0ab3b93
userhjwork <zjavbfltm@gmail.com>
2026-07-10 13:18:43 +0900

    feat: 테이블 컴포넌트 동기화(미완성)

---
+++ src/components/TableColgroup.jsx
@@ -0,0 +1,15 @@
+function TableColgroup() {
+    return(
+        <colgroup>
+          <col className='short' />
+          <col className='title' />
+          <col className='content' />
+          <col className='etc' />
+          <col className='etc' />
+          <col className='etc' />
+          <col className='etc' />
+        </colgroup>
+    )
+}
+
+export default TableColgroup;
