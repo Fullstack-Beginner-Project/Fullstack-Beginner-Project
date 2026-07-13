534f83d87333525623acc4d945da8373d0007a0c
kangyeon <dbwldhkd01@naver.com>
2026-07-13 15:11:57 +0900

    feat: 나의 기업 비교하기 기업 선택 플로우 구현 및 상세 디테일 수정

--- src/components/ModalCompanySelect.jsx
+++ src/components/ModalCompanySelect.jsx
@@ -5,12 +5,12 @@ import Search from './Search';
 import CompanyLists from '../components/CompanyLists';
 import Pagination from '../components/Pagination';
 
-import { mockCompanies } from '../mocks/companies';
+import { companies as mockCompanies } from '../mock/companies';
 
 const PAGE_SIZE = 5;
 
-function ModalCompanySelect() {
-  const [companies, setCompanies] = useState(mockCompanies);
+function ModalCompanySelect({ onClose, onSelectCompany }) {
+  const companies = mockCompanies;
   const [keyword, setKeyword] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -35,16 +35,9 @@ function ModalCompanySelect() {
   );
 
   const handleSelect = (id) => {
-    setCompanies((prev) =>
-      prev.map((company) =>
-        company.id === id
-          ? {
-              ...company,
-              selected: !company.selected,
-            }
-          : company
-      )
-    );
+    const company = companies.find((company) => company.id === id);
+    onSelectCompany(company);
+    onClose();
   };
 
   const handleKeywordChange = (value) => {
@@ -55,6 +48,7 @@ function ModalCompanySelect() {
   return (
     <Modal
       title="나의 기업 선택하기"
+      onClose={onClose}
       footer={
         <Pagination
           currentPage={currentPage}
