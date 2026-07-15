a72a0f8c420215d04df22df7db0ae448442976cd
aksths <aksths161@gmail.com>
2026-07-15 18:43:20 +0900

    feat: 목데이터 삭제 후 테스트 데이터 추가

--- src/pages/CompanyDetail.jsx
+++ src/pages/CompanyDetail.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import Table from "../components/Table";
 import Button from "../components/Button";
 import Pagination from "../components/Pagination";
-import DefaultLogo from "../assets/images/logo_default.png";
 import "../assets/css/CompanyDetail.css";
-
-import { mockCompanies } from "../mock/companies";
-// 투자 코멘트를 위한 mock 데이터 추가
-import { mockInvestments } from "../mock/investments";
+import DefaultLogo from "../assets/images/logo_default.png";
+import axios from "axios";
 
 const PAGE_SIZE = 5;
 // const MENU_WIDTH = 154;
@@ -16,11 +14,11 @@ const PAGE_SIZE = 5;
 // const GAP = 8;
 
 function CompanyDetail() {
-  const [commentMenu, setCommentMenu] = useState({
-    row: null,
-    top: 0,
-    left: 0,
-  });
+  // const [commentMenu, setCommentMenu] = useState({
+  //   row: null,
+  //   top: 0,
+  //   left: 0,
+  // });
 
   // const handleOpenCommentMenu = (event, row) => {
   //   event.stopPropagation();
@@ -54,24 +52,40 @@ function CompanyDetail() {
   //     };
   //   });
   // };
-
-  const company = mockCompanies[0];
-
+  const {companyId} = useParams();
+  const [company, setCompany] = useState(null);
+  const [investments, setInvestments] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  const totalPages = Math.max(
-    1,
-    Math.ceil(mockInvestments.length / PAGE_SIZE)
-  );
+  // API 연결
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 기업 상세
+        const companyResponse = await axios.get(
+          `https://fullstack-beginner-api-test.ggeonwoo.workers.dev/api/companies/${companyId}`
+        );
+        setCompany(companyResponse.data.company);
 
-  const paginatedInvestments = mockInvestments.slice(
-    (currentPage - 1) * PAGE_SIZE,
-    currentPage * PAGE_SIZE
-  );
+        // 투자 목록
+        const investmentResponse = await axios.get(
+          `https://fullstack-beginner-api-test.ggeonwoo.workers.dev/api/companies/${companyId}/investments?page=${currentPage}&pageSize=${PAGE_SIZE}`
+        );
+        setInvestments(investmentResponse.data.list);
+        setTotalPages(Math.ceil(investmentResponse.data.totalCount / PAGE_SIZE)
+      );
+      } catch (error) {
+        console.error("데이터 불러오기 실패:", error);
+      }
+    };
+
+    fetchData();
+  }, [companyId, currentPage]);
 
   const columnDefs = [
     {
-      key: "investor",
+      key: "investorName",
       label: "투자자 이름",
       colClassName: "etc_2",
     },
@@ -92,7 +106,9 @@ function CompanyDetail() {
     },
   ];
 
-
+  if (!company) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="content_wrap">
       <div className="company_detail_wrap">
@@ -114,12 +130,12 @@ function CompanyDetail() {
           <div className="company_summary">
             <div className="summary_box">
               <span>누적 투자 금액</span>
-              <strong>{(company.actualInvestmentAmount / 100000000).toLocaleString()}억 원</strong>
+              <strong>{Number(company.actualInvestmentAmount / 100000000).toLocaleString()}억 원</strong>
             </div>
 
             <div className="summary_box">
               <span>매출액</span>
-              <strong>{(company.revenue / 100000000).toLocaleString()}억 원</strong>
+              <strong>{Number(company.revenue / 100000000).toLocaleString()}억 원</strong>
             </div>
 
             <div className="summary_box">
@@ -147,10 +163,13 @@ function CompanyDetail() {
               기업투자하기
             </Button>
           </div>
-          <p>총 {(company.myStartupInvestmentAmount / 100000000).toLocaleString()}억 원</p>
+          <p>총 {Number(company.actualInvestmentAmount / 100000000).toLocaleString()}억 원</p>
         </div>
 
-        <Table columnDefs={columnDefs} rows={paginatedInvestments}></Table>
+        <Table 
+          columnDefs={columnDefs} 
+          rows={investments}>
+        </Table>
 
         <Pagination
           currentPage={currentPage}
