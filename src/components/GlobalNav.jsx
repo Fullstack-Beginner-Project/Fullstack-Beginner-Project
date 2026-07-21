1239347df2ec277e2938d34c280fbbb1f065cf62
kangyeon <dbwldhkd01@naver.com>
2026-07-21 13:33:51 +0900

    feat:찜한목록 페이지 생성 gnb에 연결작업

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -9,6 +9,7 @@ const NAV_ITEMS = [
   { label: '나의 기업 비교', to: '/my-company-compare' },
   { label: '비교 현황', to: '/compare-overview' },
   { label: '투자 현황', to: '/investment-status' },
+  { label: '찜한 목록', to: '/wish-list' },
 ];
 
 function GlobalNav() {
