3474a688247afc33e1bc29150ab672695ed9a434
kangyeon <dbwldhkd01@naver.com>
2026-07-21 16:24:50 +0900

    Feat: 찜한 기업 조회 API 추가

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import investmentRouter from './investment-routes.js';
 import companyRouter from './company-routes.js';
 import compareRouter from './compare-routes.js';
+import favoriteRouter from './favorite-routes.js';
 
 
 
@@ -11,6 +12,7 @@ const router = Router();
 router.use(investmentRouter);
 router.use(companyRouter);
 router.use(compareRouter);
+router.use(favoriteRouter);
 
 
 
