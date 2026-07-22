5fa32ee95a6443f2c7edd1fac390c8b70e3f198b
Happy-DCS <ronawam@gmail.com>
2026-07-22 18:21:26 +0900

    feat: 이미지 추가

--- src/pages/CompareResult.jsx
+++ src/pages/CompareResult.jsx
@@ -5,7 +5,6 @@ import LogoImg from "../components/LogoImg.jsx";
 import Section from "../components/Section";
 import Dropdown from "../components/Dropdown";
 import Button from "../components/Button";
-import LogoImg from "../components/LogoImg.jsx";
 import "../assets/css/compareResult.css";
 import ModalConfirm from "../components/ModalConfirm.jsx";
 import Table from "../components/Table";
