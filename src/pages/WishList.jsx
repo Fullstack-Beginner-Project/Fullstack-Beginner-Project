b01070b55cdadd85c0de59f586876f27c8ce9bdd
kangyeon <dbwldhkd01@naver.com>
2026-07-24 15:41:01 +0900

    fix:찜한 목록 페이지 드롭박스 컴포넌트 반응형 수정

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -5,6 +5,7 @@ import Table from "../components/Table.jsx";
 import Pagination from "../components/Pagination.jsx";
 import axios from "../api/axios.js";
 import { getFavoriteCompanyIds } from "../utils/common"
+import "../assets/css/wishlist.css";
 
 
 const columnDefs = [
